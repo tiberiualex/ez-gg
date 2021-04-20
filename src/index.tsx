5c29dd7e76ff57b40d9ebaffcabcad67be910a1b
tiberiualex <tiberiu.alexander@gmail.com>
2021-04-20 03:41:24 +0300

    More Redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/store';
+import store from './state/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
